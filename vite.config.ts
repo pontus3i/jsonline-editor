a89c204ca67af76b68d2d08c232f5143b7cf08ef
pontus3i <tonykms210@gmail.com>
2024-05-08 14:08:02 +0700

    Github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+  base: '/jsonline-editor/',
   plugins: [solid()],
+  build: {
+    outDir: 'docs',
+  },
 })
